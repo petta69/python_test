057b93a468c8734e4397504e765a446859c76ce2
Peter Stridsberg <peter.stridsberg@chyronhego.com>
2018-10-16 14:29:44 +0200

    More test

--- first/qwe.py
+++ first/qwe.py
@@ -5,7 +5,12 @@ import os
 import numpy as np
 
 def main():
-    print("QWE")
+    print("QWE2")
+    test(null,20)
+
+def test(qwe='hej', id=10):
+    print "QWE: {} {}".format(qwe, id)
+
 
 if __name__ == "__main__":
     main()
