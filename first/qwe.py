88a3912c042f2282240921442b3484e406e98f09
peter <petta@fightstone.com>
2018-10-17 20:29:40 +0200

    Added argparse

--- first/qwe.py
+++ first/qwe.py
@@ -3,9 +3,16 @@
 import sys
 import os
 import numpy as np
+import argparse
 
 def main():
-    print("QWE")
+    parser = argparse.ArgumentParser(description='Testing testing.')
+    parser.add_argument('--test', type=str)
+
+    args = parser.parse_args()
+
+
+    print "Hello {}".format(args.test)
     print "QWE: {}".format(np.__version__)
 
 if __name__ == "__main__":
