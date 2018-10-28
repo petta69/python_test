71fffe3c565948b8316e44ee368e5ca78d47dc40
peter <petta@fightstone.com>
2018-10-28 17:42:40 +0100

    Last changes

--- first/qwe.py
+++ first/qwe.py
@@ -17,11 +17,7 @@ def main():
     print "Peter {}".format(args.test)
     print "QWE: {}".format(np.__version__)
 
-    # set up orthographic map projection with
-    # perspective of satellite looking down at 50N, 100W.
-    # use low resolution coastlines.
-    map = Basemap(projection='cyl',llcrnrlon=40,llcrnrlat=40, urcrnrlon=60, urcrnrlat=71,resolution='l')#,
-    #llcrnrx=-90,llcrnry=0,urcrnrx=90.,urcrnry=90)
+    map = Basemap(projection='cyl',llcrnrlon=0,llcrnrlat=50, urcrnrlon=30, urcrnrlat=75,resolution='l')#,
     # draw coastlines, country boundaries, fill continents.
     map.drawcoastlines(linewidth=0.25)
     map.drawcountries(linewidth=0.25)
@@ -29,17 +25,8 @@ def main():
     # draw the edge of the map projection region (the projection limb)
     map.drawmapboundary(fill_color='aqua')
     # draw lat/lon grid lines every 30 degrees.
-    map.drawmeridians(np.arange(0,360,30))
-    map.drawparallels(np.arange(-90,90,30))
-    # make up some data on a regular lat/lon grid.
-    nlats = 73; nlons = 145; delta = 2.*np.pi/(nlons-1)
-    lats = (0.5*np.pi-delta*np.indices((nlats,nlons))[0,:,:])
-    lons = (delta*np.indices((nlats,nlons))[1,:,:])
-    wave = 0.75*(np.sin(2.*lats)**8*np.cos(4.*lons))
-    mean = 0.5*np.cos(2.*lats)*((np.sin(2.*lats))**2 + 2.)
-    # compute native map projection coordinates of lat/lon grid.
-    x, y = map(lons*180./np.pi, lats*180./np.pi)
-    # contour data over the map.
+    map.drawmeridians(np.arange(0,360,10))
+    map.drawparallels(np.arange(-90,90,10))
     #cs = map.contour(x,y,wave+mean,15,linewidths=1.5)
     plt.title('contour lines over filled continent background')
     plt.show()
